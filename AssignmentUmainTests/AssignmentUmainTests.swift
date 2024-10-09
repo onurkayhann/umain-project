f82ce7c684d2882b1d571a43d1f0559b7b1d2722
Onur <onur.kayhan90@gmail.com>
2024-10-09 22:13:42 +0200

    Initial Commit

---
+++ AssignmentUmainTests/AssignmentUmainTests.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentUmainTests.swift
+//  AssignmentUmainTests
+//
+//  Created by Onur Kayhan on 2024-10-09.
+//
+
+import Testing
+@testable import AssignmentUmain
+
+struct AssignmentUmainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
